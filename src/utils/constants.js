38cb237e65123d91afcadca863238f4205634578
olukukoyi <oluwanifesimi.kukoyi15@gmail.com>
2022-09-08 15:16:36 -0400

    first commit

---
+++ src/utils/constants.js
@@ -0,0 +1,4 @@
+import abi from "./Transactions.json";
+
+export const contractABI = abi.abi;
+export const contractAddress = "0x41986683e0A974c6BB2fdFbe54660F92F88c8f72";
---
+++ tailwind.config.js
@@ -0,0 +1,40 @@
+module.exports = {
+  content: [
+    // Example content paths...
+    "./public/**/*.html",
+    "./src/**/*.{js,jsx,ts,tsx,vue}",
+  ],
+  mode: "jit",
+  darkMode: "media", // or 'media' or 'class'
+  theme: {
+    fontFamily: {
+      display: ["Open Sans", "sans-serif"],
+      body: ["Open Sans", "sans-serif"],
+    },
+    extend: {
+      screens: {
+        mf: "990px",
+      },
+      keyframes: {
+        "slide-in": {
+          "0%": {
+            "-webkit-transform": "translateX(120%)",
+            transform: "translateX(120%)",
+          },
+          "100%": {
+            "-webkit-transform": "translateX(0%)",
+            transform: "translateX(0%)",
+          },
+        },
+      },
+      animation: {
+        "slide-in": "slide-in 0.5s ease-out",
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+  // plugins: [require("@tailwindcss/forms")],  this line causes an error
+};
